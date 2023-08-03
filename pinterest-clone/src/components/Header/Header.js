43b747b52a9f2366592c790d9f9f11cbb252fcb7
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-08-03 02:56:35 -0300

    add header component

---
+++ pinterest-clone/src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css';
+
+export const Header = (props) =>{
+  return (
+   <header>
+    <h1>Pinterest</h1>
+    <nav>
+     <ul>
+      <li>
+       <a href="">Home</a>
+      </li>
+      <li>
+       <a href="">Minhas Pastas</a>
+      </li>
+     </ul>
+    </nav>
+   </header>
+  );
+}
+
+export default Header;
