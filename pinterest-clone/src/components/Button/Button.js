af41e8d1811d7bc299820703da52deddbd203922
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-08-03 02:08:45 -0300

    add:button component

---
+++ pinterest-clone/src/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Button.css';
+import Badge from '../Badge/Badge';
+
+export const Button = () =>{
+  return (
+  <button className="button">
+    Salvar
+    <Badge/>
+  </button>)
+}
+export default Button
