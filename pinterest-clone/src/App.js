a6116290f5a6daca52d6aeca3673b0560084123d
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-08-02 21:26:23 -0300

    add bootstrap

--- pinterest-clone/src/App.js
+++ pinterest-clone/src/App.js
@@ -1,9 +1,11 @@
-
+import React from "react";
+import { Home } from "./pages/Home";
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+ return (
+  <div className="App">
+   <Home />
+  </div>
+ );
 }
 
 export default App;
