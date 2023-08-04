a87bdcafbca0b5a70fedf4306b2a151070f7b189
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-08-04 03:28:38 -0300

    Update App.js

--- pinterest-clone/src/App.js
+++ pinterest-clone/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Home } from './pages/Home'
 import Header from './partials/Header/Header'
+import './App.css'
 function App () {
   return (
     <div className='App'>
