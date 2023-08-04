42b18d6d827625dee05ed3ee45cfc795d654c183
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-08-04 03:29:56 -0300

    Update Home.js

--- pinterest-clone/src/pages/Home.js
+++ pinterest-clone/src/pages/Home.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import { PinCard } from '../components/PinCard/PinCard'
+import Modal from '../components/Modal/Modal'
 export const Home = () => {
   return (
     <>
+      <Modal isOpen={true} 
+      title={'Salvar Pin'}
+      controls={[
+        {
+        label:'Criar Pasta',
+        onClick:() => {console.log('clicou em salvar pasta')}
+        },
+        {
+        label:'Salvar Pin',
+        onClick:() => {console.log('clicou em salvar pin')}
+        }
+      ]}
+      >
+        <p>Home</p>
+      </Modal>
       <PinCard title='title' total={9} image='bhb' />
     </>
   )
