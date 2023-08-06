dfbd8f410b6e3c073a414bf40da1d01253429ed3
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-08-05 23:44:35 -0300

    add: modalCreateFolder

---
+++ pinterest-clone/src/containers/ModalCreateFolder/ModalCreateFolder.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import Modal from '../../components/Modal/Modal';
+import './ModalCreateFolder.css'
+
+const ModalCreateFolder = ({ isOpen }) => {
+const [folderName, setFolderName] = useState('')
+
+  const handleSubmit = (e) =>{
+      e.preventDefault();
+      console.log('criando...', folderName)
+     }
+ const handleChange = (e) =>{
+    setFolderName(e.target.value)
+ }
+  return (
+    <Modal
+     title='Criar e Salvar'
+     isOpen={isOpen}
+     controls = {[
+      {
+        label: 'Criar e salvar',
+        loadingLabel:'salvando...',
+        type:'submit',
+        form:'form-criar-pasta'
+      }
+     ]} 
+    >
+      <form onSubmit={handleSubmit} id='form-criar-pasta'>
+        <label>Nome da pasta</label>
+        <input type="text" placeholder='ex.: matemática' value={folderName} onChange={handleChange}/>
+      </form>
+    </Modal>
+  )
+}
+export default ModalCreateFolder
