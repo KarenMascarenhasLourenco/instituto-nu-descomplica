0314b0cb3d2c61f9c6825b27be12f375864a7e4c
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-08-05 23:54:52 -0300

    add: Alert

---
+++ pinterest-clone/src/components/Alert/Alert.js
@@ -0,0 +1,12 @@
+import ReactDOM  from 'react-dom'
+import './Alert.css'
+
+const Alert = ({ message, onClose }) => {
+  return ReactDOM.createPortal(
+   <div className="alert-notification">
+    <p>{message} </p>
+    <div type='button'>X</div>
+   </div>, document.body
+  );
+}
+export default Alert
