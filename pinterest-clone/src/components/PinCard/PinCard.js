bdc9a148762a036e9bfb6aeb4728536317c408b4
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-08-10 23:01:10 -0300

    Update PinCard.js

--- pinterest-clone/src/components/PinCard/PinCard.js
+++ pinterest-clone/src/components/PinCard/PinCard.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import './PinCard.css'
-import { Button } from '../Button/Button'
-import { Badge } from '../Badge/Badge'
+import React from "react";
+import "./PinCard.css";
+import { Button } from "../Button/Button";
+import { Badge } from "../Badge/Badge";
 export const PinCard = ({ image, title, total }) => {
-  return (
-    <div>
-      <div className='card-img'>
-        <img src={image} alt='' />
-        <div className='overlay'>
-          <p className=''>pasta</p>
-          <Button>
-            <Badge total={total} />
-          </Button>
-          <p className=''>{image}</p>
-        </div>
-      </div>
-      <div className='card-content'>
-        <h3 className='card-title'>{title}</h3>
-        <p className='card-description'>descrição</p>
-      </div>
+ return (
+  <div>
+   <div className="card-img">
+    <img src={image} alt="" />
+    <div className="overlay">
+     <p className="">pasta</p>
+     <Button>
+      <Badge total={total} />
+     </Button>
+     <p className="">{image}</p>
     </div>
-  )
-}
+   </div>
+   <div className="card-content">
+    <h3 className="card-title">{title}</h3>
+    <p className="card-description">descrição</p>
+   </div>
+  </div>
+ );
+};
 
-export default PinCard
+export default PinCard;
