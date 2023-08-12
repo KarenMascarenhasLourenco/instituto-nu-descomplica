60377fe40a194fbec127ecb5c176b09ce6efd5b8
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-08-12 02:53:48 -0300

    Update Card.js

--- pinterest-clone/src/containers/CardContainer/Card.js
+++ pinterest-clone/src/containers/CardContainer/Card.js
@@ -3,10 +3,10 @@ import { useAppContext } from "../../store/AppContext";
 import { openModalSavePinAction } from "../../store/Action";
 const Card = (props) => {
   const { state, dispatch} = useAppContext()
-  const handleClick= () => {
+  const handleClick= (pinId) => {
     console.log('aeeeeeeeee ta dando certo')
-    dispatch(openModalSavePinAction())
-    console.log({ state })
+    dispatch(openModalSavePinAction(pinId))
+    console.log({ state }, pinId)
   }
   return(
     <PinCard {...props} onClick={handleClick}/>
