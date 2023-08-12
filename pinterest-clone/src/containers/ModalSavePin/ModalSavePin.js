200bded21fbf7c56f24761de4eeb989b3ebb93cd
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-08-12 02:53:56 -0300

    Update ModalSavePin.js

--- pinterest-clone/src/containers/ModalSavePin/ModalSavePin.js
+++ pinterest-clone/src/containers/ModalSavePin/ModalSavePin.js
@@ -2,38 +2,45 @@ import { useEffect } from 'react'
 import Modal from '../../components/Modal/Modal'
 import { useAppContext } from '../../store/AppContext'
 import { fetchFoldersAction, closeModalsAction, openModalCreateFolderAction } from '../../store/Action'
+
 const ModalSavePin = ({ isOpen }) => {
   const { state,  dispatch } = useAppContext()
+
   const handleClose = () =>{
-    console.log('to fecahndooooooo')
     dispatch(closeModalsAction())
   }
+
   const handleClickCreateFolder= ()=>{
-    console.log('criando pastaaaa')
     dispatch(openModalCreateFolderAction())
   }
-  useEffect(()=>{
+
+  useEffect(() => {
     fetchFoldersAction(dispatch)
   },[])
+
   return (
-    <Modal
-      title='Salvar Pin'
-      isOpen={isOpen}
-      className='modal-content'
-      overlayClassName='modal-overlay'
-      handleClose={handleClose}
-      controls={[
-        {
-          label: 'Criar pastas',
-          onClick: handleClickCreateFolder,
-          loading: false,
-          loadinglabel: 'Salvando pin',
-          disable: 'disable'
-        }
-      ]}
-    >
-      {console.log(state.folders)}
-    </Modal>
-  )
+   <Modal
+    title="Salvar Pin"
+    isOpen={isOpen}
+    className="modal-content"
+    overlayClassName="modal-overlay"
+    handleClose={handleClose}
+    controls={[
+     {
+      label: "Criar pastas",
+      onClick: handleClickCreateFolder,
+      loading: false,
+      loadinglabel: "criando",
+      disable: "disable",
+     },
+    ]}
+   >
+    <select>
+     {state.folders.map((folder, folderIndex) => (
+      <option key={folderIndex}>{folder.name}</option>
+     ))}
+    </select>
+   </Modal>
+  );
 }
 export default ModalSavePin
