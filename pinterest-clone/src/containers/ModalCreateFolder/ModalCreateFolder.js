aa93238f24715c060a3046821e4b4c4a842d3533
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-08-12 02:53:52 -0300

    Update ModalCreateFolder.js

--- pinterest-clone/src/containers/ModalCreateFolder/ModalCreateFolder.js
+++ pinterest-clone/src/containers/ModalCreateFolder/ModalCreateFolder.js
@@ -1,43 +1,55 @@
-import React, { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Modal from '../../components/Modal/Modal'
 import './ModalCreateFolder.css'
 import { useAppContext } from '../../store/AppContext'
-import { closeModalsAction } from '../../store/Action'
-
+import { closeModalsAction, saveFolderAction } from '../../store/Action'
+import { saveFolderInitType, saveFolderSucessType } from '../../store/Types'
 const ModalCreateFolder = ({ isOpen }) => {
-  const {state, dispatch} = useAppContext()
+  const { state, dispatch } = useAppContext()
   const [folderName, setFolderName] = useState('')
 
+  const handleClose = () => {
+   dispatch(closeModalsAction());
+  };
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('criando...', folderName)
+    saveFolderAction(dispatch,folderName)
+    
   }
-   const handleClose = () => {
-    console.log("to fecahndooooooo");
-    dispatch(closeModalsAction());
-   };
   const handleChange = (e) => {
     setFolderName(e.target.value)
   }
+  useEffect(() => {
+   if (state.type === saveFolderSucessType) {
+    handleClose()
+   }
+  }, [state.type]);
+
   return (
-    <Modal
-      title='Criar e Salvar'
-      isOpen={isOpen}
-      handleClose={handleClose}
-      controls={[
-        {
-          label: 'Criar e salvar',
-          loadinglabel: 'salvando...',
-          type: 'submit',
-          form: 'form-criar-pasta'
-        }
-      ]}
-    >
-      <form onSubmit={handleSubmit} id='form-criar-pasta'>
-        <label>Nome da pasta</label>
-        <input type='text' placeholder='ex.: matemática' value={folderName} onChange={handleChange} />
-      </form>
-    </Modal>
-  )
+   <Modal
+    title="Criar e Salvar"
+    isOpen={isOpen}
+    handleClose={handleClose}
+    controls={[
+     {
+      label: "Criar e salvar",
+      loadinglabel: "salvando...",
+      type: "submit",
+      form: "form-criar-pasta",
+      loading: state.type === saveFolderInitType,
+     },
+    ]}
+   >
+    <form onSubmit={handleSubmit} id="form-criar-pasta">
+     <label>Nome da pasta</label>
+     <input
+      type="text"
+      placeholder="ex.: matemática"
+      value={folderName}
+      onChange={handleChange}
+     />
+    </form>
+   </Modal>
+  );
 }
 export default ModalCreateFolder
