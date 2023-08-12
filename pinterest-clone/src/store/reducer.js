5d1af7f7fe8993f25f0ddcde9272b395f8a8315f
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-08-12 02:54:16 -0300

    Update reducer.js

--- pinterest-clone/src/store/reducer.js
+++ pinterest-clone/src/store/reducer.js
@@ -5,24 +5,41 @@ export function reducer(state, action) {
   case types.openModalSavePinType:
    return {
     ...state,
+    type:types.openModalSavePinType,
     mode: "savePin",
+    activePinId: action.payload,
+   };
+  case types.openModalCreateFolderType:
+   return {
+    ...state,
+    type: types.openModalCreateFolderType,
+    mode: "createFolder",
    };
   case types.closeModalsType:
    return {
     ...state,
     mode: null,
    };
-   case types.fetchFoldersSucessType:
-    return{
-      ...state,
-      folders:action.payload
-    }
-    case types.openModalCreateFolderType:
-      return{
-        ...state,
-        mode:'createFolder'
-      }
+  //----------
+  case types.fetchFoldersSucessType:
+   return {
+    ...state,
+    folders: action.payload,
+   };
+  case types.fetchFoldersInitType:
+   return {
+    ...state,
+    type: types.fetchFoldersInitType,
+   };
+  case types.saveFolderSucessType:
+   return {
+    ...state,
+    folders: [...state.folders, action.payload],
+   };
   default:
-   return state;
+   return {
+    ...state,
+    type:action.type
+   };
  }
 }
