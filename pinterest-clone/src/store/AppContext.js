bc0d58c8cfb0efaf9403aebd12e9719416bcf837
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-08-12 02:54:11 -0300

    Update AppContext.js

--- pinterest-clone/src/store/AppContext.js
+++ pinterest-clone/src/store/AppContext.js
@@ -1,11 +1,12 @@
 import { createContext, useContext, useReducer } from 'react'
 import { reducer } from './reducer'
+
 export const Context = createContext({})
 
 export const useAppContext = () => useContext(Context)
 
-export const AppContext = ({ children, initalState }) =>{
-  const [ state, dispatch ] = useReducer(reducer, initalState)
+export const AppContext = ({ children, initialState }) =>{
+  const [ state, dispatch ] = useReducer(reducer, initialState)
 
   return(
     <Context.Provider value={{ state, dispatch }} >
