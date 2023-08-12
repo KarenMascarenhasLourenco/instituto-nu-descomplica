573c2244a417e7a2a3c1e1d2014a40c030927d90
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-08-12 02:54:20 -0300

    Update Types.js

--- pinterest-clone/src/store/Types.js
+++ pinterest-clone/src/store/Types.js
@@ -5,3 +5,7 @@ export const openModalCreateFolderType='createFolder'
 export const fetchFoldersType = 'fetch folders'
 export const fetchFoldersInitType = 'fetch folders init'
 export const fetchFoldersSucessType = "fetch folders sucess";
+
+export const saveFolderType = "saveFolder";
+export const saveFolderInitType = "save folder init";
+export const saveFolderSucessType = "saveFolder sucess";
