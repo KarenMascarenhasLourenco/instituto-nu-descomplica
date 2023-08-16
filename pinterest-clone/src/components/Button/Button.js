26c3ba9d498c3035c4b7d0bf4aae5bb9bcbcbca8
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-08-16 01:30:44 -0300

    Update Button.js

--- pinterest-clone/src/components/Button/Button.js
+++ pinterest-clone/src/components/Button/Button.js
@@ -1,9 +1,9 @@
 import './Button.css'
 import Spinner from '../Spinner/Spinner'
 
-const Button = ({ label, loading, children, loadinglabel, ...buttonProps }) => {
+const Button = ({ label, loading, children, loadinglabel, disabled, ...buttonProps }) => {
   return (
-    <button className='button buttonAction' {...buttonProps}>
+    <button className='button buttonAction' {...buttonProps} disabled={disabled}>
       <Spinner loading={loading} />
       {loading ? loadinglabel : label}
       {children}
